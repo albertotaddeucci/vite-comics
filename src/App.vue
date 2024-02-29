7efdc41a6ccfad7753eb8580438b9488b21845e3
alberto <alberto.taddeucci@gmail.com>
2024-02-29 14:05:15 +0100

    completamento navabr

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import AppFooter from './components/AppFooter.vue';
 export default {
   components: {
     AppNav,
-    AppFooter
+    AppFooter,
   }
 }
 
--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,6 +1,12 @@
 <script>
 export default {
     name: "AppNav",
+
+    data() {
+        return {
+            links: ["Characters", "Comics", "Movies", "Tv", "Games", "Collectibles", "Videos", "Fans", "News", "Shop"]
+        }
+    }
 }
 
 
@@ -8,8 +14,41 @@ export default {
 
 <template>
     <nav>
-        nav
+        <div class="container">
+
+            <div>
+                <img src="/img/dc-logo.png" alt="">
+            </div>
+            <div>
+                <ul>
+                    <li v-for="link in links">
+                        {{ link }}
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
     </nav>
 </template>
 
-<style></style>
+<style>
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    max-width: 1260px;
+    margin-inline: auto;
+    padding-block: 20px;
+
+    ul {
+        display: flex;
+        gap: 2em;
+
+        list-style: none;
+        text-transform: uppercase;
+    }
+
+}
+</style>
